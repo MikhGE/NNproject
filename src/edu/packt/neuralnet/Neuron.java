b0e13f95cb547ac97643b3d8035f53f7354f8b0f
MikhGE <mikhge@yandex.ru>
2022-04-27 11:41:21 +0300

    Neuron class

---
+++ src/edu/packt/neuralnet/Neuron.java
@@ -0,0 +1,45 @@
+package edu.packt.neuralnet;
+
+import java.util.ArrayList;
+
+public class Neuron {
+    protected   ArrayList<Double>   weight;
+    private     ArrayList<Double>   input;
+    private     Double              output;
+    private     Double              outputBeforeActivation;
+    private     int                 numberOfInputs = 0;
+    protected   Double              bias = 1.0;
+    private     IActivationFunction activationFunction;
+
+    public Neuron(int numberofinputs, IActivationFunction iaf) {
+        numberOfInputs      = numberofinputs;
+        weight              = new ArrayList<>(numberofinputs + 1);
+        input               = new ArrayList<>(numberofinputs);
+        activationFunction  = iaf;
+    }
+
+    public void init() {
+        for (int i = 0; i <= numberOfInputs; i++) {
+            double newWeight = RandomNumberGenerator.GenerateNext();
+            try {
+                this.weight.set(i, newWeight);
+            } catch (IndexOutOfBoundsException iobe) {
+                this.weight.add(newWeight);
+            }
+        }
+    }
+
+    public void calc() {
+        outputBeforeActivation = 0.0;
+        if (numberOfInputs > 0) {
+            if (input != null && weight != null) {
+                for (int i = 0; i <= numberOfInputs; i++) {
+                    outputBeforeActivation += (i == numberOfInputs ? bias : input.get(i)) * weight.get(i);
+                }
+            }
+        }
+        output = activationFunction.calc(outputBeforeActivation);
+    }
+
+}
+
